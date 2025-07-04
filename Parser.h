cb02e5f2f8b1cf7341ea9f34b1d8ba8ddba913ca
Jongmyeong Choi <cheesechoi@gmail.com>
2025-07-05 07:46:20 +0900

    Lecture 2

---
+++ Parser.h
@@ -0,0 +1,47 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "AST.h"
+#include "Lexer.h"
+#include "llvm/Support/raw_ostream.h"
+
+class Parser {
+    Lexer &Lex;
+    Token Tok;
+    bool HasError;
+
+    void error() {
+        llvm::errs() << "Unexpected: " << Tok.getText() << "\n";
+        HasError = true;
+    }
+
+    void advance() { Lex.next(Tok); }
+
+    bool expect(Token::TokenKind Kind) {
+        if (Tok.getKind() != Kind) {
+            llvm::errs() << "err: " << Tok.getText() << "\n";
+            error();
+            return true;
+        }
+        return false;
+    }
+
+    bool consume(Token::TokenKind Kind) {
+        if (expect(Kind))
+            return true;
+        advance();
+        return false;
+    }
+
+    AST *parseCalc();
+    Expr *parseExpr();
+    Expr *parseTerm();
+    Expr *parseFactor();
+    
+public:
+    Parser(Lexer &l) : Lex(l), HasError(false) { advance(); }
+    bool hasError() const { return HasError; }
+    AST *parse();
+};
+
+#endif
